cb8d20f7577b820f4b8cd02b9245d5d3844714b0
Kuminoti <danielkum02@gmail.com>
2024-06-25 16:33:27 +0200

    Added macros and Multithreading

--- include/macros.hpp
+++ include/macros.hpp
@@ -7,10 +7,15 @@
     #include <vector>
     #include <functional>
     #include <stdint.h>
+    #include <algorithm>
+    #include <thread>
+    #include <chrono>
 
 
-    #define STDRES 0.1f               //The standard resolution for more dimensional vector systems
-    #define ZERO 0                    //Standard zero used as a constant for the startpoint in more dimensional vector systems
+
+
+    #define STDRES 0.1f                //The standard resolution for more dimensional vector systems
+    #define ZERO 0                     //Standard zero used as a constant for the startpoint in more dimensional vector systems
     #define PI 3.14159265358979323846
     #define EULER 2.718281828459045
     #define TWOPI 2 * PI
@@ -21,12 +26,20 @@
     #define NAMESPACESTART namespace Dmath {
     #define NAMESPACEEND }
 
+    #define uint8_t byte 
+    
+
     #ifdef WORKING
         #include <iostream>
         #define LOGGER(info){ std::cout<< __FILE__ << " " << __LINE__ << " " << info <<std::endl;}
     #endif //WORKING
     
 
+    void intervallThreadMain(uint16_t millis){
+        //Add functions for your second thread
+        std::this_thread::sleep_for(std::chrono::milliseconds(millis));
+    }
 
+    std::thread intervallThread(intervallThreadMain,100);
 
 #endif //DK_MACROS_HPP
