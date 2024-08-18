21a031e37ec0cf2a9a577be379f46f5dcf272d76
Kuminoti <danielkum02@gmail.com>
2024-08-18 10:38:44 +0200

    Mooved specific data to datatypes.hpp

--- include/systemGeometry.hpp
+++ include/systemGeometry.hpp
@@ -9,21 +9,13 @@
 
 NAMESPACESTART
 
+
+
+class Vec2D;
 class Vec3D;
 
 
-struct Triangle2D{
-  Dmath::Vec2D tri[3];
-};
 
-struct Triangle3D{
-  Dmath::Vec3D tri[3];
-};
-
-struct Square2D{
-  Dmath::Vec2D sqr[4];
-
-};
 
 /* This class can be used to calculate gemometric data from
  * forms in coordinatesystems 
--- include/systems.hpp
+++ include/systems.hpp
@@ -4,7 +4,7 @@
 #define SYSTEMS_H
 
 #include"macros.hpp"
-
+#include"dataTypes.hpp"
 
 /* 
  * This file is used to create a base and an abstraction for the other classes and systems.
@@ -16,67 +16,7 @@
  * mathmatical constants and data.
 */
 
-NAMESPACESTART
-#pragma region Datatypes
-template<typename d_type_one, typename d_type_two>
-struct Duo {
-    d_type_one one;
-    d_type_two two;
 
-    // Constructor using initializer list
-    Duo(d_type_one one, d_type_two two)
-        : one(one), two(two) {}
-
-    // Default constructor
-    Duo() : one(), two() {}
-
-    template<typename T = d_type_one, typename std::enable_if<std::is_same<T, d_type_two>::value, int>::type = 0>
-    constexpr void swap() {
-        d_type_one val = this->two;
-        this->two = this->one;
-        this->one = val;
-    }
-    
-};
-
-
-template<typename d_type_one, typename d_type_two, typename d_type_three>
-struct Trio{
-
-  d_type_one   one;
-  d_type_two   two;
-  d_type_three three;
-
-  Trio(d_type_one one, d_type_two two, d_type_three three) 
-      : one(one), two(two), three(three) {}
-
-
-  Trio(): one(), two(), three() {}
-
-  template<typename T = d_type_one, typename std::enable_if<std::is_same<T,d_type_two>::value, int>::type = 0>
-  constexpr void swapOneAndTwo(){
-    d_type_one val = this->two;
-    this->two = this->one;
-    this->one = val;
-  }
-  template<typename T = d_type_one, typename std::enable_if<std::is_same<T,d_type_three>::value, int>::type = 0>
-  constexpr void swapOneAndThree(){
-    d_type_one val = this->three;
-    this->three = this->one;
-    this->one = val;
-  }
-
-  template<typename T = d_type_two, typename std::enable_if<std::is_same<T,d_type_three>::value, int>::type = 0>
-  constexpr void swapTwoAndThree(){
-    d_type_three val = this->three;
-    this->three = this->two;
-    this->two = val;
-  }
-};
-
-
-#pragma endregion 
-NAMESPACEEND
 
 
 #ifdef SYSTEM_READY
