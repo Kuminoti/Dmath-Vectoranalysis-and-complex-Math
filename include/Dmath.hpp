5d1e8e8a237157e6f98b8632164984a1209ce617
Kuminoti <danielkum02@gmail.com>
2024-08-18 10:38:21 +0200

    Added new Librarys in the Dmath header

--- include/Dmath.hpp
+++ include/Dmath.hpp
@@ -2,11 +2,12 @@
 
 #ifndef DMATH_H
 #define DMATH_H
-
+#include"dataTypes.hpp"
 #include"mathVector.hpp"
 #include"analysis.hpp"
 #include"basicFunction.hpp"
 #include"VectorCurve.hpp"
+#include"Matrix.hpp"
 
 
 
