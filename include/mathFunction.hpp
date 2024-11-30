6f9e3e1b97aea58b4accaa00e5e1f7fce751f285
Kuminoti <danielkum02@gmail.com>
2024-11-30 17:39:45 +0100

    Deleted mathFunction and replaced it by something better

---
+++ include/mathFunction.hpp
@@ -0,0 +1,325 @@
+#pragma once 
+
+#ifndef DMATH_FUNCTIONAL_HPP
+#define DMATH_FUNCTIONAL_HPP
+
+
+
+/*
+ * mathFunction.h
+ *
+ * This file defines a custom function wrapper that mimics the behavior 
+ * of std::function, specifically optimized for handling mathematical 
+ * functions represented as double lambda expressions.
+ * 
+ * Example: f(x) = x² ; code [](double x) ->double { return (x*x); };
+ * 
+ * The primary  purpose of this class is to provide a flexible and type-safe
+ * way to store and invoke mathematical functions that take a single double 
+ * argument and return a double value.
+ *
+ * Key Features:
+ * - Supports any callable that matches the signature `double(double)`.
+ * - Provides an interface for invoking stored functions.
+ * - Can be easily integrated into existing C++ codebases.
+ */
+
+
+
+
+
+#include"macros.hpp"
+#include"dataTypes.hpp"
+#include<memory>
+#include<windows.h>
+
+
+
+NAMESPACESTART
+
+
+
+#pragma region BASECLASS
+
+//Base class for the other classes
+
+
+// Basis-Klasse für Funktionen
+// Base class for functions
+class FunctionBase {
+public:
+    virtual ~FunctionBase() = default;
+    virtual double Callx(double x) = 0;             // For f(x)
+    virtual double CallXY(double x, double y) = 0;  // For f(x, y)
+    virtual double CallXYZ(double x, double y, double z) = 0; //For f(x,y,z)
+};
+
+#pragma endregion
+
+#pragma region FUNCWRAPPER
+
+// Template class for function functionality
+template<typename FuncX>
+class FunctionWrapper : public FunctionBase {
+private:
+    FuncX singleFunction;       // Function for f(x)
+
+public:
+    // Constructor for single variable function
+    FunctionWrapper(FuncX funX) : singleFunction(funX) {}
+
+    double Callx(double x) override {
+        return singleFunction(x); // Call for f(x)
+    }
+
+    double CallXY(double x, double y) override {
+        throw std::runtime_error("Single variable function does not support CallXY."); // Not implemented for single variable function
+    }
+
+    double CallXYZ(double x, double y, double z) override {
+        throw std::runtime_error("Single variable function does not support CallXY."); // Not implemented for single variable function
+    }
+
+};
+
+template<typename FuncXY>
+class FunctionWrapperXY : public FunctionBase {
+private:
+    FuncXY doubleFunction;      // Function for f(x, y)
+
+public:
+    // Constructor for double variable function
+    FunctionWrapperXY(FuncXY funXY) : doubleFunction(funXY) {}
+
+    double Callx(double x) override {
+        throw std::runtime_error("This function does not support Callx."); // Not implemented for double variable function
+    }
+
+    double CallXY(double x, double y) override {
+        return doubleFunction(x, y); // Call for f(x, y)
+    }
+    double CallXYZ(double x, double y, double z) override {
+        throw std::runtime_error("Single variable function does not support CallXY."); // Not implemented for single variable function
+    }
+};
+
+template<typename FuncXYZ>
+class FunctionWrapperXYZ : public FunctionBase{
+private:
+    FuncXYZ tripleFunc;
+
+public:
+    FunctionWrapperXYZ(FuncXYZ funXYZ) : tripleFunc(funXYZ) {}
+
+        double Callx(double x) override {
+        throw std::runtime_error("This function does not support Callx."); // Not implemented for double variable function
+    }
+
+    double CallXY(double x, double y) override {
+        throw std::runtime_error("This function does not support Callxy."); // Not implemented for double variable function
+    }
+    double CallXYZ(double x, double y, double z) override {
+        return tripleFunc(x,y,z);
+    }
+};
+
+#pragma endregion
+
+#pragma region SINGLE
+
+class SHARED_LIB SingleVarFunction {
+
+private: //Private members:
+    std::unique_ptr<FunctionBase> funcBase; // Smart pointer for memory management
+   
+    const double dx = 0.0000001;
+
+    
+
+private: //private methods: 
+
+    bool checkParams(Dmath::Parameters params);
+
+
+public: //public getters
+    double getDataAt(double x){ return this->funcBase->Callx(x);  }
+
+
+public: //operator overloading
+
+
+    /* Calculates the number of elements for exapmle if you want
+     * to create the derivative 
+     * Parameters is a datatype wich holds 3 values:
+     * @param 1. param.one   = start
+     * @param 2. param.two   = end
+     * @param 3. param.three = stepps or resolution
+     */
+    size_t numOfElements(Dmath::Parameters param){
+
+
+        double result = 0;
+
+        result = static_cast<size_t>((param.two - param.one)/param.three);
+        
+        return result;
+    }
+
+public:
+    template<typename Callable>
+    SingleVarFunction(Callable func) : funcBase(std::make_unique<FunctionWrapper<Callable>>(func)) {}
+
+    SingleVarFunction() = default;
+
+ 
+
+    double operator()(double x) {
+        if (funcBase) {
+            return funcBase->Callx(x); // Delegate the call to the stored function
+        }
+        return 0.0; // Return a default value if no function is stored
+    }
+
+  
+
+/**
+ * This method, getFunctionVector, returns a vector filled with all function values
+ * from a given start point (argument one) to an end point (argument two) in specified steps
+ * (argument three).
+ *
+ * @param start The starting point for the function evaluations.
+ * @param stopp The end point for the function evaluations.
+ * @param stepps The step size between evaluations.
+ * @return A vector containing the function values. Returns an empty vector if 
+ *         the start point is greater than or equal to the stop point or if
+ *         the step size is non-positive.
+ */
+
+    std::vector<double> getFunctionVector(double start, double stopp, double stepps);
+
+
+
+    /*
+ * This method, getDerivativeVector, computes the derivative of a mathematical function 
+ * represented by a callable object stored in funcBase. The derivative is calculated using 
+ * the central difference method, which approximates the derivative by evaluating the 
+ * function at points slightly above and below a given x-value (using a small delta, dx).
+ * 
+ * Parameters:
+ * - start: The starting point for the derivative calculation.
+ * - stopp: The endpoint for the derivative calculation.
+ * - stepps: The step size between successive x-values.
+ *
+ * Returns:
+ * A vector containing the computed derivative values at each step from 'start' to 'stopp'.
+ * If the input parameters are invalid (start >= stopp or stepps <= 0), an empty vector is returned.
+ */
+
+    std::vector<double> getDerivativeVector(double start, double stopp, double stepps);
+
+/*
+ * This method calculates the approximate anti-derivative (integral) values of a mathematical function
+ * over a specified range defined by 'start' and 'stopp' with specified step sizes 'stepps'.
+ * 
+ * @param start   The starting point of the integration range.
+ * @param stopp   The endpoint of the integration range.
+ * @param stepps  The step size for discretizing the range into intervals.
+ * 
+ * @return       A vector of double values representing the accumulated integral values at each step.
+ * 
+ * The method checks the validity of the input parameters and returns an empty vector if they are invalid.
+ * It utilizes the trapezoidal rule for numerical integration, accumulating the area under the curve
+ * of the function defined by the stored function pointer (funcBase).
+ * Each integral value is calculated by evaluating the function at the current x value and
+ * multiplying it by the differential element 'dx'.
+ */
+
+std::vector<double> getAntiDerivativeVector(double start, double stopp, double stepps);
+
+};
+
+#pragma region DOUBLE
+
+class SHARED_LIB DoubleVarFunction {
+private:
+    std::unique_ptr<FunctionBase> funcBase; // Smart pointer for the function
+   
+    const double dx = 0.0000001;
+
+private:
+    bool checkParams(Dmath::Parameters params);
+
+public:
+    template<typename FuncXY>
+    DoubleVarFunction(FuncXY func) : funcBase(std::make_unique<FunctionWrapperXY<FuncXY>>(func)) {} // Wrapper for f(x, y)
+
+    double operator()(double x, double y) {
+        if (funcBase) {
+            return funcBase->CallXY(x, y); // Delegate call to stored function
+        }
+        return 0.0; // Default value
+    }
+
+
+    size_t numOfElements(Dmath::Parameters params);
+
+
+
+    std::vector<double> getFunctionVector (double start, double stopp, double stepps);
+
+    std::vector<double> getPartialDerivteX(double start, double stopp, double stepps);
+    std::vector<double> getPartialDerivteY(double start, double stopp, double stepps);
+
+    std::vector<double> getAntiDerivativeX(double start, double stopp, double stepps);
+    std::vector<double> getAntiDerivativeY(double start, double stopp, double stepps);
+
+};
+
+#pragma endregion //double
+
+
+#pragma region TRIPPLE
+class SHARED_LIB TripleVarFunction{
+private:
+    std::unique_ptr<FunctionBase> funcBase;
+    const double dx = 0.0000001;
+
+
+    bool checkParams(Dmath::Parameters params);
+    size_t numberOfElements(Dmath::Parameters params);
+
+public:
+    template<typename FuncXYZ>
+    TripleVarFunction(FuncXYZ func) : funcBase(std::make_unique<FunctionWrapperXYZ<FuncXYZ>>(func)){};
+
+    double operator()(double x, double y, double z) {
+        if (funcBase) {
+            return funcBase->CallXYZ(x, y, z); // Delegate call to stored function
+        }
+        return 0.0; // Default value
+    }
+
+
+
+    std::vector<double> getFunctionVector (double start, double stopp, double stepps);
+
+    std::vector<double> getPartialDerivteX(double start, double stopp, double stepps);
+    std::vector<double> getPartialDerivteY(double start, double stopp, double stepps);
+    std::vector<double> getPartialDerivteZ(double start, double stopp, double stepps);
+
+
+    std::vector<double> getAntiDerivativeX(double start, double stopp, double stepps);
+    std::vector<double> getAntiDerivativeY(double start, double stopp, double stepps);
+    std::vector<double> getAntiDerivativeZ(double start, double stopp, double stepps);
+
+
+};
+
+
+#pragma endregion //Tripple
+
+NAMESPACEEND
+
+
+
+#endif //DMATH_FUNCTIONAL_HPP
---
+++ src/mathFunction.cpp
@@ -0,0 +1,399 @@
+#include"../include/mathFunction.hpp"
+
+#pragma region SingleVar
+
+bool Dmath::SingleVarFunction::checkParams(Dmath::Parameters params){
+        if(params.one >= params.two || params.three == 0){
+            return 0;
+        }
+    return true;
+    }
+
+
+
+
+
+std::vector<double>  Dmath::SingleVarFunction::getFunctionVector(double start, double stopp, double stepps){
+    //Error checking: 
+
+    if(start >= stopp || stepps == 0){
+        return std::vector<double>(0);
+    }
+
+    Dmath::Parameters params(start,stopp,stepps);
+    const size_t num = this->numOfElements(params);
+ 
+    std::vector<double> mainVec;
+
+    for(size_t i = 0; i < num; i++){
+        double currentResult = funcBase->Callx((start + i*stepps));
+        mainVec.push_back(currentResult);
+    }
+    return mainVec;
+}
+
+
+std::vector<double> Dmath::SingleVarFunction::getDerivativeVector(double start, double stopp, double stepps){
+
+    //It,s simpler to use a struct in stet of all args inside the class 
+    Dmath::Parameters params(start,stopp,stepps);
+    
+    if(!this->checkParams(params)){
+        return std::vector<double>(0);
+    }
+
+    //Number of elements
+    const size_t num = this->numOfElements(params);
+    
+    std::vector<double> mainVector;
+
+    for(size_t i = 0; i < num; i++){
+
+        double x = start + i * stepps; // Current x value
+        double plusDX  = this->funcBase->Callx(x + dx); // f(x + dx)
+        double minusDX = this->funcBase->Callx(x - dx); // f(x - dx)
+        double result  = (plusDX - minusDX) / (2 * dx); // Central difference method
+
+        mainVector.push_back(result); // Store the result
+
+    }
+    return mainVector;
+}
+
+
+std::vector<double> Dmath::SingleVarFunction::getAntiDerivativeVector(double start, double stopp, double stepps) {
+    Dmath::Parameters param(start, stopp, stepps);
+    if (!this->checkParams(param)) {
+        return std::vector<double>(0);
+    }
+
+    // Number of intervals
+    size_t num = this->numOfElements(param);
+    std::vector<double> mainVec;
+    double integral = this->funcBase->Callx(start);
+    mainVec.push_back(integral);
+    // Numerical integration over the interval
+    for (size_t i = 0; i < num; i++) {
+        double currentX = start + i * stepps;
+        double nextX = currentX + stepps;
+
+        // Trapezoidal rule: Average of function values at currentX and nextX
+        double avgValue = 0.5 * (this->funcBase->Callx(currentX) + this->funcBase->Callx(nextX));
+        
+        integral += avgValue * stepps;   // Step size * average value
+        mainVec.push_back(integral);     // Store the integrated value
+    }
+
+    return mainVec;
+}
+
+
+#pragma endregion 
+
+#pragma region DoubleVar
+
+
+bool Dmath::DoubleVarFunction::checkParams(Dmath::Parameters params){
+
+    if(params.one >= params.two || params.three == 0){
+        return false;
+    }
+    return true;
+}
+
+std::vector<double> Dmath::DoubleVarFunction::getFunctionVector(double start, double stopp, double stepps){
+    Dmath::Parameters params(start,stopp,stepps);
+    if(!this->checkParams(params)){
+        return std::vector<double>(0);
+    }
+        const size_t num = static_cast<size_t>((stopp-start)/stepps);
+
+        std::vector<double> mainVec;
+        for(size_t x = 0; x < num; x++){
+            double currentX = (start + x * stepps);
+            for(size_t y = 0; y < num; y++){
+                double currentY = (start + y * stepps);
+
+                double currentData = this->funcBase->CallXY(currentX,currentY);
+
+                mainVec.push_back(currentData);
+            }
+        }
+
+        return mainVec;
+    }
+
+
+std::vector<double> Dmath::DoubleVarFunction::getPartialDerivteX(double start, double stopp, double stepps){
+    Dmath::Parameters params(start,stopp,stepps);
+    if(!this->checkParams(params)){
+        return std::vector<double>(0);
+    }
+    const size_t num = static_cast<size_t>((stopp-start)/stepps);
+
+    std::vector<double> mainVec;
+
+    for(size_t i = 0; i < num; i++){
+        double currentX = (start + i * stepps);
+        for(size_t j = 0; j < num ; j++){
+            double currentY = start+j*stepps;
+            double dxPlus   = this->funcBase->CallXY(currentX + dx, currentY);
+            double dxMinus  = this->funcBase->CallXY(currentX - dx, currentY);
+            double result   = ((dxPlus - dxMinus)/(2*dx));
+            mainVec.push_back(result); 
+        }
+        
+    } 
+    return mainVec;
+}
+
+
+std::vector<double> Dmath::DoubleVarFunction::getPartialDerivteY(double start, double stopp, double stepps){
+    Parameters params(start,stopp,stepps);
+    if(!this->checkParams(params)){
+        return std::vector<double>(0);
+    }
+    const size_t num = static_cast<size_t>((stopp-start)/stepps);
+
+    std::vector<double> mainVec;
+
+    for(size_t i = 0; i < num; i++){
+        double currentX = (start + i * stepps);
+        for(size_t j = 0; j < num ; j++){
+            double currentY = start+j*stepps;
+            double dxPlus   = this->funcBase->CallXY(currentX, currentY + dx);
+            double dxMinus  = this->funcBase->CallXY(currentX, currentY - dx);
+
+            double result   = ((dxPlus - dxMinus)/(2*dx));
+            mainVec.push_back(result); 
+        }
+        
+    } 
+    return mainVec;
+}
+
+
+
+std::vector<double> Dmath::DoubleVarFunction::getAntiDerivativeX(double start, double stopp, double stepps) {
+    Dmath::Parameters params(start, stopp, stepps);
+    if (!this->checkParams(params)) {
+        return std::vector<double>(0);
+    }
+
+    size_t num = this->numOfElements(params);
+    std::vector<double> mainVec;
+    double integral = this->funcBase->CallXY(start, start);
+    mainVec.push_back(integral);
+
+    // Numerical integration over the interval in X direction
+    for (size_t i = 0; i < num; i++) {
+        double currentX = start + i * stepps;
+        double nextX = currentX + stepps;
+        
+        for (size_t j = 0; j < num; j++) {
+            double currentY = start + j * stepps;
+
+            // Trapezoidal rule: Average of function values at currentX and nextX
+            double avgValue = 0.5 * (this->funcBase->CallXY(currentX, currentY) + this->funcBase->CallXY(nextX, currentY));
+            
+            integral += avgValue * stepps;
+            mainVec.push_back(integral); // Store the integrated value after each X interval
+        }
+        
+    }
+    return mainVec;
+}
+
+std::vector<double> Dmath::DoubleVarFunction::getAntiDerivativeY(double start, double stopp, double stepps) {
+    Dmath::Parameters params(start, stopp, stepps);
+    if (!this->checkParams(params)) {
+        return std::vector<double>(0);
+    }
+
+    size_t num = this->numOfElements(params);
+    std::vector<double> mainVec;
+    double integral = this->funcBase->CallXY(start, start);
+    mainVec.push_back(integral);
+
+    // Numerical integration over the interval in Y direction
+    for (size_t i = 0; i < num; i++) {
+        double currentX = start + i * stepps;
+        
+        for (size_t j = 0; j < num; j++) {
+            double currentY = start + j * stepps;
+            double nextY = currentY + stepps;
+
+            // Trapezoidal rule: Average of function values at currentY and nextY
+            double avgValue = 0.5 * (this->funcBase->CallXY(currentX, currentY) + this->funcBase->CallXY(currentX, nextY));
+            
+            integral += avgValue * stepps;
+            mainVec.push_back(integral); // Store the integrated value after each Y interval
+
+        }
+    }
+    return mainVec;
+}
+
+
+
+size_t Dmath::DoubleVarFunction::numOfElements(Dmath::Parameters params){
+    if( !this->checkParams(params)){
+        return 0;
+    }
+    size_t num = static_cast<size_t>((params.two - params.one)/params.three);
+    return num;
+}
+
+#pragma endregion
+
+
+
+#pragma region TrippleVar
+
+
+bool Dmath::TripleVarFunction::checkParams(Dmath::Parameters params){
+    if(params.one >= params.two || params.three == 0 ){
+        return false;
+    }
+    return true;
+}
+
+size_t Dmath::TripleVarFunction::numberOfElements(Dmath::Parameters params){
+    if(!this->checkParams(params)){ return 0; }
+
+    size_t num = static_cast<size_t>((params.two - params.one)/params.three);
+    return num;
+}
+
+std::vector<double> Dmath::TripleVarFunction::getFunctionVector(double start, double stopp, double stepps){
+    Dmath::Parameters params(start,stopp,stepps);
+    if(!this->checkParams(params)){
+        return std::vector<double>(0);
+    }
+    const size_t num = this->numberOfElements(params);
+
+    std::vector<double> funcVector;
+
+    for(size_t x = 0; x < num; x++){
+        double currentX = start + x * stepps;
+
+        for(size_t y = 0; y < num; y++){
+            double currentY = start + y * stepps;
+
+            for(size_t z = 0; z < num; z++){
+                double currentZ = start + z * stepps;
+
+                double currentResult = this->funcBase->CallXYZ(currentX,currentY,currentZ);
+
+                funcVector.push_back(currentResult);
+            }
+        }
+    }
+    return funcVector;
+}
+
+
+
+std::vector<double> Dmath::TripleVarFunction::getPartialDerivteX(double start, double stopp, double stepps) {
+    Dmath::Parameters params(start, stopp, stepps);
+    if (!this->checkParams(params)) {
+        return std::vector<double>(0);
+    }
+    const size_t num = this->numberOfElements(params);
+
+    std::vector<double> funcVector;
+    funcVector.reserve(num * num * num);
+
+    for (size_t x = 0; x < num; x++) {
+        double currentX = (start + x * stepps);
+
+        for (size_t y = 0; y < num; y++) {
+            double currentY = (start + y * stepps);
+
+            for (size_t z = 0; z < num; z++) {
+                double currentZ = (start + z * stepps);  // Korrigiert auf z
+
+                double xDX = this->funcBase->CallXYZ(currentX + this->dx, currentY, currentZ);
+                double Xdx = this->funcBase->CallXYZ(currentX - this->dx, currentY, currentZ);
+
+                double result = (xDX - Xdx) / (2 * this->dx);  // Klammern hinzugefügt
+                funcVector.push_back(result);
+            }
+        }
+    }
+
+    return funcVector;
+}
+
+
+
+
+
+
+
+std::vector<double> Dmath::TripleVarFunction::getPartialDerivteY(double start, double stopp, double stepps) {
+    Dmath::Parameters params(start, stopp, stepps);
+    if (!this->checkParams(params)) {
+        return std::vector<double>(0);
+    }
+    const size_t num = this->numberOfElements(params);
+
+    std::vector<double> funcVector;
+    funcVector.reserve(num * num * num);
+
+    for (size_t x = 0; x < num; x++) {
+        double currentX = (start + x * stepps);
+
+        for (size_t y = 0; y < num; y++) {
+            double currentY = (start + y * stepps);
+
+            for (size_t z = 0; z < num; z++) {
+                double currentZ = (start + z * stepps);  // Korrigiert auf z
+
+                double yDY = this->funcBase->CallXYZ(currentX, currentY + this->dx, currentZ);
+                double Ydy = this->funcBase->CallXYZ(currentX, currentY - this->dx, currentZ);
+
+                double result = (yDY - Ydy) / (2 * this->dx);  // Klammern hinzugefügt
+                funcVector.push_back(result);
+            }
+        }
+    }
+
+    return funcVector;
+}
+
+
+
+std::vector<double> Dmath::TripleVarFunction::getPartialDerivteZ(double start, double stopp, double stepps) {
+    Dmath::Parameters params(start, stopp, stepps);
+    if (!this->checkParams(params)) {
+        return std::vector<double>(0);
+    }
+    const size_t num = this->numberOfElements(params);
+
+    std::vector<double> funcVector;
+    funcVector.reserve(num * num * num);
+
+    for (size_t x = 0; x < num; x++) {
+        double currentX = (start + x * stepps);
+
+        for (size_t y = 0; y < num; y++) {
+            double currentY = (start + y * stepps);
+
+            for (size_t z = 0; z < num; z++) {
+                double currentZ = (start + z * stepps);  // Korrigiert auf z
+
+                double zDZ = this->funcBase->CallXYZ(currentX, currentY, currentZ + this->dx);
+                double Zdz = this->funcBase->CallXYZ(currentX, currentY, currentZ - this->dx);
+
+                double result = (zDZ - Zdz) / (2 * this->dx);  // Klammern hinzugefügt
+                funcVector.push_back(result);
+            }
+        }
+    }
+
+    return funcVector;
+}
+
+
+#pragma endregion
