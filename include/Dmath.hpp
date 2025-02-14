3b0f0e86e478d6d7beeedb3d2de6eb385976d3a9
Kuminoti <danielkum02@gmail.com>
2025-02-14 19:14:29 +0100

    comments

--- include/Dmath.hpp
+++ include/Dmath.hpp
@@ -3,13 +3,14 @@
 #ifndef DMATH_H
 #define DMATH_H
 
-#include"dataTypes.hpp"
-#include"mathVector.hpp"
-#include"VectorCurve.hpp"
-#include"vectorSurface.hpp"
-#include"Matrix.hpp"
-#include"dFunctions.hpp"
-#include"mathFunction.hpp"
+#include"dataTypes.hpp"         //Contains small and basic datatypes
+#include"mathVector.hpp"        //2D and 3D Vectors
+#include"VectorCurve.hpp"       //Parametric curves
+#include"vectorSurface.hpp"     //Parametric surface
+#include"Matrix.hpp"            //Matrices
+#include"dFunctions.hpp"        //Helperfunctions and functional math
+#include"mathFunction.hpp"      //Mathmatic funktion objects
+#include"complex.hpp"           //complex numbers
 
 
 
