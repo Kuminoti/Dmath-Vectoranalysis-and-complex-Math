f0dd90dcad7019d84d78af8a3a8968c1886487f2
Kuminoti <danielkum02@gmail.com>
2025-04-20 19:55:38 +0200

    added more typetraids

--- include/Types/traits.hpp
+++ include/Types/traits.hpp
@@ -7,10 +7,38 @@
 #include"../complex.hpp"
 NAMESPACESTART
 
-template<typename T>
+template<typename Com>
 struct is_Complex{
-    static constexpr bool value = std::is_same<t,Dmath::Complex>::value
+    static constexpr bool value = std::is_same<Com,Dmath::Complex>::value
 };
+
+
+template<typename Func>
+struct is_function_object{
+    static constexpr bool value = std::is_same<Func ,Dmath::SingleVarFunction>::value || 
+                                  std::is_same<Func ,Dmath::DoubleVarFunction>::value ||
+                                  std::is_same<Func ,Dmath::TripleVarFunction>::value;
+};
+
+
+template<typename Vec2>
+struct is_vec2{
+    static constexpr bool value = std::is_same<Vec2, Dmath::Vec2D >::value ||
+                                  std::is_same<Vec2, Dmath::sVec2f>::value ||
+                                  std::is_same<Vec2, Dmath::sVec2i>::value;
+};
+
+
+template<typename Vec3>
+
+struct is_vec3{
+    static constexpr bool value = std::is_same<Vec2, Dmath::Vec3D >::value ||
+                                  std::is_same<Vec2, Dmath::sVec3f>::value ||
+                                  std::is_same<Vec2, Dmath::sVec3i>::value;
+};
+
+
+
 
 NAMESPACEEND
 #endif
